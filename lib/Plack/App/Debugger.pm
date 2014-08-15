15d5df3700cc92bfe003e0921791bb8d357b1b91
Stevan Little <stevan.little@booking.com>
2014-08-15 11:22:02 +0200

    stop using Plack::Util::Accessor, I don’t actually want rw accessors

--- lib/Plack/App/Debugger.pm
+++ lib/Plack/App/Debugger.pm
@@ -3,13 +3,24 @@ package Plack::App::Debugger;
 use strict;
 use warnings;
 
+use Scalar::Util qw[ blessed ];
 use parent 'Plack::Component';
 
-use Plack::Util::Accessor (
-    'debugger', # a reference to the Plack::Debugger
-);
+sub new {
+    my $class = shift;
+    my %args  = @_ == 1 && ref $_[0] eq 'HASH' ? %{ $_[0] } : @_;
+
+    die "You must pass a reference to a 'Plack::Debugger' instance"
+        unless blessed $args{'debugger'} 
+            && $args{'debugger'}->isa('Plack::Debugger');
+
+    $class->SUPER::new( %args );
+}
 
 sub prepare_app {}
+# accessors ...
+
+sub debugger { (shift)->{'debugger'} } # a reference to the Plack::Debugger
 
 sub call {
     my ($self, $env) = @_;
